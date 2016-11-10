acf3b2b24dfb1542ebc4813dd993ba2c964179e4
navdeep-G <mr.navdeepgill@gmail.com>
2016-11-09 17:12:14 -0800

    Update theme

--- app.R
+++ app.R
@@ -7,6 +7,7 @@ suppressMessages (library(tidyr))
 suppressMessages (library(dplyr))
 suppressMessages (library(Hmisc))
 suppressMessages (library(quantreg))
+suppressMessages (library(shinythemes))
 
 
 stat_sum_df <- function(fun, geom="point", ...) {
@@ -25,7 +26,7 @@ tableau20 <- c("#1F77B4","#AEC7E8", "#FF7F0E","#FFBB78"  ,"#2CA02C",
                "#8C564B","#C49C94" ,"#E377C2","#F7B6D2" ,"#7F7F7F",
                "#C7C7C7" ,"#BCBD22","#DBDB8D" ,"#17BECF","#9EDAE5")
 
-ui  <-  fluidPage(
+ui  <-  fluidPage(theme = shinytheme("cyborg"),
   titlePanel("Explore Data"),
   sidebarLayout(
     sidebarPanel(
